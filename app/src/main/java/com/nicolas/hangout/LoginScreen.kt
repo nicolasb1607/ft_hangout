92a8666937ed13e380025bfd0b92440f8270a1e3
Nicolas Burat <nicolasburat1607@gmail.com>
2023-10-30 12:52:09 +0100

    Moving color to value color ressources

--- app/src/main/java/com/nicolas/hangout/LoginScreen.kt
+++ app/src/main/java/com/nicolas/hangout/LoginScreen.kt
@@ -24,7 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -81,7 +81,7 @@ fun FilledButton(textValue: String, modifier: Modifier = Modifier) {
     Button(
         onClick = { null },
         modifier = modifier,
-        colors = ButtonDefaults.buttonColors(Color(0xff6563ff))
+        colors = ButtonDefaults.buttonColors(colorResource(R.color.main))
     ) {
         Text( text = textValue)
     }
